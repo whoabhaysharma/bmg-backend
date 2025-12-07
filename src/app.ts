a36c774cf624756ad407d41061a72424f7472845
Abhay Sharma <abhay@techcrumb.com>
2025-12-07 19:29:48 +0530

    fix the cors

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,10 @@ app.use(express.json({
     }
 }));
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors({
+    origin: ['https://app.gyms24.in', 'http://localhost:3000', 'http://localhost:3001'],
+    credentials: true
+}));
 app.use(helmet());
 app.use(morgan('dev'));
 

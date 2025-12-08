5920a013530fd2e773e2a6b02b1d265b9fff6d54
Abhay Sharma <abhay@techcrumb.com>
2025-12-08 19:40:29 +0530

    build: Integrate  into the build process and enable all CORS origins.

--- src/app.ts
+++ src/app.ts
@@ -14,7 +14,7 @@ app.use(express.json({
 }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-    origin: ['https://app.gyms24.in', 'http://localhost:3000', 'http://localhost:3001'],
+    origin: true, // Allow all origins
     credentials: true
 }));
 app.use(helmet());
